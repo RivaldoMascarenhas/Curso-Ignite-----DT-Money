ddd92d9062a21ea0b0dc08507e17d771ecb7df4a
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2022-11-22 09:06:03 -0300

    First Commit

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,16 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+                <Dialog.Title>Nova Transação</Dialog.Title>
+                <Dialog.Close></Dialog.Close>
+            </Dialog.Content>
+        </Dialog.Portal>
+    )
+}
